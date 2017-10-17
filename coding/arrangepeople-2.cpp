79f90409922a10e165ab392414452b5ced5db531
yao2001626 <yao2001626@gmail.com>
2017-10-17 11:13:59 -0400

    Create arrangepeople-2.cpp

---
+++ coding/arrangepeople-2.cpp
@@ -0,0 +1,39 @@
+/*
+ find the min number and distance to the last position. ret = min*n + distance
+*/
+#include <iostream>
+#include <cstdlib>
+int arrange(int arr[], int last_pos){
+
+
+}
+
+int main() {
+    int n,x;
+    int i = 0;
+    unsigned int *arr;
+    scanf("%d, %d", &n, &x);
+    arr = (unsigned int *)malloc(sizeof(unsigned int)*n);
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    int tmp = 0;
+    for(int j=x-1; ; --j){//
+        if(j < 0){
+            j = j + n;
+        }
+        if(arr[j] >= 0){
+            arr[j] -= 1;
+            tmp++;
+        }else{
+            arr[j] = tmp;
+     		break;
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 1;
+}
