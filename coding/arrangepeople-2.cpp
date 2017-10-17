f794fc54ee1a2b992219a61656c375d4fe9594bd
yao2001626 <yao2001626@gmail.com>
2017-10-17 11:59:01 -0400

    Update arrangepeople-2.cpp

--- coding/arrangepeople-2.cpp
+++ coding/arrangepeople-2.cpp
@@ -1,39 +1,61 @@
 /*
  find the min number and distance to the last position. ret = min*n + distance
 */
+// ConsoleApplication3.cpp : Defines the entry point for the console application.
+//
+
 #include <iostream>
 #include <cstdlib>
-int arrange(int arr[], int last_pos){
+#include <vector>
+using namespace std;
+int distance(int fpos, int lpos, int length) {
+	return fpos <= lpos ? lpos - fpos : length - fpos + lpos;
+}
+void arrange(int arr[], int last_pos, int length) {
+	//find min and index
+	int min = arr[0];
+	int index = 0;
+	for (size_t i = 1; i < length; i++)
+	{
+		if (arr[i] < min) {
+			min = arr[i];
+			index = i;
+		}
+	}
+	int tol = length*min + distance(index+1, last_pos, length);
+	for (size_t i = 0; i < length; i++)
+	{
+		if (i == index) {//index = total
+			arr[i] = tol;
+		}
+		else if(distance(i+1,last_pos, length)<= tol%length ){//dis <= tol%length
+			arr[i] -= min + 1;
+		}
+		else {//dis == index
 
-
+			arr[i] -= min;
+		}
+	}
+	return;
 }
 
-int main() {
-    int n,x;
-    int i = 0;
-    unsigned int *arr;
-    scanf("%d, %d", &n, &x);
-    arr = (unsigned int *)malloc(sizeof(unsigned int)*n);
-    for(i=0; i<n; i++){
-        scanf("%d", &arr[i]);
-    }
-    int tmp = 0;
-    for(int j=x-1; ; --j){//
-        if(j < 0){
-            j = j + n;
-        }
-        if(arr[j] >= 0){
-            arr[j] -= 1;
-            tmp++;
-        }else{
-            arr[j] = tmp;
-     		break;
-        }
-    }
-    
-    for(int i=0;i<n;i++){
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    return 1;
+int main()
+{
+	int n, x;
+	int ret = 0;
+	cin >> n >> x ;
+	int *array = new int[n];
+	for (size_t i = 0; i < n; i++)
+	{
+		cin >> array[i];
+	}
+	arrange(array, x, n);
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << array[i] << ' ';
+	}
+	cout << endl;
+
+	return 0;
 }
+
